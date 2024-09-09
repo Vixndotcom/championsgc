1eea810ecc5b656200f5a04f90f65a74e07667f5
alex aku <vixndotcom@gmail.com>
2024-09-09 09:31:15 +0100

    vp

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -11,14 +11,14 @@ const Hero = () => {
      <section className={`${styles.flexStart} `} >
        <div className='mt-20 mb-20'>
  
-       <h1 className=' mt-20 text-shadow text-start text-white' >
+       <h1 className=' mt-20 text-shadow text-start text-white mb-20' >
          <div className='mb-5 galgo tracking-wide text-7xl'>Stimulating the  <span className='purple'>Mind</span> </div>
          <div className='mb-5 galgo tracking-wide text-7xl '>and <span className='purple'> Unravelling difficult</span> </div>
          <div className='mb-5 galgo tracking-wide text-7xl'>Thought Processes </div>
       </h1>
 
 {/* central div */}
-<div className='md:flex md:flex-row md:gap-16 z-[999] mb-20'>
+<div className='md:flex md:flex-row md:gap-20 z-[999] mb-20 '>
       <div className='bg-book w-[320px] h-[auto] rounded-[12px]  mt-5 ' >
        <div className='flex flex-row text-center mt-10 bg-video w-[300px] h-[140px]'>
        <div className='text-[24px] mx-8  mt-5 neue-thin '>
